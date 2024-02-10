1e617ad57ac39e0dd8990bdfe7aabf3ca3929ba4
Alberes Pereira <alberesbass@hotmail.com>
2024-02-10 13:55:06 -0300

    primeiro commit do projeto

---
+++ src/js/painel.js
@@ -0,0 +1,38 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+function esconderImagens() {
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+      });
+}
+
+function mostrarImagem() {
+    imagensPainel[imagemAtual].classList.add('mostrar');
+}
+
+setaAvancar.addEventListener('click', function() { 
+    
+    const totalDeImagens = imagensPainel.length -1;
+    if(imagemAtual === totalDeImagens) {
+        return;
+    }
+
+   imagemAtual++;
+   
+   esconderImagens();
+   mostrarImagem();
+});
+
+setaVoltar.addEventListener('click', function() {
+
+    if(imagemAtual === 0) {
+        return;
+    }
+    imagemAtual--;
+
+  esconderImagens();
+  mostrarImagem();
+});
